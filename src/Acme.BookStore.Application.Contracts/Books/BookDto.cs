bb4f19205e36fc948089dcabf07484d5df7f1e58
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-05-15 18:36:48 +0200

    Add BookDto

---
+++ src/Acme.BookStore.Application.Contracts/Books/BookDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Books
+{
+    public sealed class BookDto : AuditedEntityDto<Guid>
+    {
+        public string? Name { get; set; }
+        public BookType Type { get; set; }
+        public DateTime PublishDate { get; set; }
+        public float Price { get; set; }
+    }
+}
